b7fa2d0927d63f43e896fc7b27883293d13af8be
Brian Kung <github@briankung.xyz>
2020-09-15 10:59:14 -0500

    Output an HTML table instead of JSON

--- Gemfile
+++ Gemfile
@@ -7,6 +7,6 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra", '~>2.1.0'
-gem "sinatra-contrib", '~>2.1.0'
 gem 'httparty', '~>0.18.1'
+gem 'thamble', '~>1.1.0'
 gem 'puma'
--- idph_stats.rb
+++ idph_stats.rb
@@ -1,52 +1,34 @@
 require 'sinatra'
-require 'sinatra/json'
 require 'httparty'
-require 'csv'
+require 'thamble'
 
 IDPH_DATA_URL = "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1".freeze
 SELECT_COUNTIES = %w{Illinois Cook Lake}.freeze
+SELECT_HEADERS = %w{confirmed_cases total_tested deaths}
 
 get '/' do
-  response = HTTParty.get(IDPH_DATA_URL, headers: {'Content-Type' => 'application/json'}).parsed_response
+  response = HTTParty.get(IDPH_DATA_URL).parsed_response
 
-  result = response['historical_county']['values'].each_with_object({}) do |date, accumulator|
-    accumulator[date['testDate']] = date['values'].filter_map do |county|
+  result = response['historical_county']['values'].map do |date|
+    row = date['values'].filter_map do |county|
       next unless SELECT_COUNTIES.include? county['County']
       county_name = county.delete('County').downcase
-      county.reject! {|k,_| k == 'lat' || k == 'lon'}
+      county.select! {|k,_| SELECT_HEADERS.include?(k)}
       county.transform_keys! {|k| "#{county_name}_#{k}"}
       county
-    end.reduce(&:merge!)
+    end
+
+    row.unshift(date: date['testDate'])
+
+    row.reduce(&:merge!)
   end
 
-  json result
+  table = Thamble.table(result.map(&:values), {
+    headers: result.first.keys,
+    table: {style: 'border: 2px solid black;border-collapse: collapse;'},
+    th: {style: 'border: thin solid black;'},
+    td: {style: 'border: thin solid black;'},
+  })
+
+  table
 end
-
-__END__
-
-I want total tested, confirmed cases, for all dates, for the state, and for lake and cook counties
-
-DATE, STATE TOTAL TESTED, STATE CONFIRMED CASES, COOK TOTAL TESTED, COOK CONFIRMED CASES, LAKE TOTAL TESTED, LAKE CONFIRMED CASES
-
-{
-  "testDate": "9/14/2020",
-  "total_tested": 4771796,
-  "confirmed_cases": 262744,
-  "deaths": 8314
-}
-{
-  "County": "Cook ",
-  "confirmed_cases": 59865,
-  "deaths": 2218,
-  "total_tested": 866565,
-  "lat": 42.050784,
-  "lon": -87.963759
-}
-{
-  "County": "Lake",
-  "confirmed_cases": 15818,
-  "deaths": 481,
-  "total_tested": 221426,
-  "lat": 42.3689,
-  "lon": -87.8272
-}
