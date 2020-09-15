aa3c2595e6db154b4f45c0561f6196bec857ca8c
Brian Kung <github@briankung.xyz>
2020-09-15 14:56:24 -0500

    Make the tables collapsible. Hack. Ontop. Of. Hack.

--- idph_stats.rb
+++ idph_stats.rb
@@ -3,14 +3,25 @@ require "sinatra/reloader" if development?
 require 'httparty'
 require 'thamble'
 
-IDPH_DATA_URL = "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1".freeze
+IDPH_COVID_TEST_DATA = "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1".freeze
+IDPH_COVID_HOSPITAL_DATA = "https://www.dph.illinois.gov/sitefiles/COVIDHospitalRegions.json?nocache=1".freeze
 SELECT_COUNTIES = %w{Illinois Cook Lake}.freeze
-SELECT_HEADERS = %w{confirmed_cases total_tested deaths}
+SELECT_HEADERS = %w{confirmed_cases total_tested deaths}.freeze
+DEFAULT_TABLE_ATTRIBUTES = {
+  table: {style: 'border: 2px solid black;border-collapse: collapse;'},
+  th: {style: 'border: thin solid black;'},
+  td: {style: 'border: thin solid black;'},
+}.freeze
 
 get '/' do
-  response = HTTParty.get(IDPH_DATA_URL).parsed_response
+  hospital_data = HTTParty.get(IDPH_COVID_HOSPITAL_DATA).parsed_response
+  test_data = HTTParty.get(IDPH_COVID_TEST_DATA).parsed_response
 
-  result = response['historical_county']['values'].map do |date|
+  region_10_hospitalization = hospital_data['regionValues'].find {|region| region['id'] == 10}.reject! {|k,_| k =~ /^(region|id)$/}
+  state_hospitalization = hospital_data['statewideValues']
+  state_hospitalization_historic = hospital_data['HospitalUtilizationResults']
+
+  test_results = test_data['historical_county']['values'].map do |date|
     row = date['values'].filter_map do |county|
       next unless SELECT_COUNTIES.include? county['County']
       county_name = county.delete('County').downcase
@@ -24,12 +35,51 @@ get '/' do
     row.reduce(&:merge!)
   end
 
-  table = Thamble.table(result.map(&:values), {
-    headers: result.first.keys,
-    table: {style: 'border: 2px solid black;border-collapse: collapse;'},
-    th: {style: 'border: thin solid black;'},
-    td: {style: 'border: thin solid black;'},
+  region_10_table = Thamble.table(region_10_hospitalization, DEFAULT_TABLE_ATTRIBUTES)
+  state_hospitalization_table = Thamble.table(state_hospitalization, DEFAULT_TABLE_ATTRIBUTES)
+  state_hospitalization_historic_table = Thamble.table(state_hospitalization_historic.map(&:values).reverse, {
+    headers: state_hospitalization_historic.first.keys,
+    **DEFAULT_TABLE_ATTRIBUTES,
+    table: {style: 'border: 2px solid black;border-collapse: collapse;', class: :collapsible},
+  })
+  test_results_table = Thamble.table(test_results.map(&:values), {
+    headers: test_results.first.keys,
+    **DEFAULT_TABLE_ATTRIBUTES,
+    table: {style: 'border: 2px solid black;border-collapse: collapse;', class: :collapsible},
   })
 
-  table
+  <<~HTML
+    <h1>Region 10</h1>
+    #{region_10_table}
+
+    <h1>Statewide Hospitalization Data</h1>
+    #{state_hospitalization_table}
+
+    <h1>Historic Statewide Hospitalization Data</h1>
+    #{state_hospitalization_historic_table}
+
+    <h1>Historic Testing Data</h1>
+    #{test_results_table}
+
+    <script type="text/javascript">
+      const toggleDisplay = (el) => {
+        el.style.display = (el.style.display == 'none' ? null : 'none');
+
+      };
+
+      const collapseTable = (e) => {
+        const table = e.target.closest('table');
+        [...table.querySelectorAll('tbody tr:not(:first-child)')].forEach(toggleDisplay);
+        e.preventDefault();
+      };
+
+      const collapsibleTables = [...document.querySelectorAll('table.collapsible')];
+
+      collapsibleTables.forEach(table => {
+        const tableContents = [...table.querySelectorAll('*')];
+        tableContents.forEach(el => el.onclick = collapseTable);
+        tableContents[0].click();
+      });
+    </script>
+  HTML
 end
