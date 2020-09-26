7ac5055adf7ee579fe8fd670a82d261623b80cad
Brian Kung <github@briankung.xyz>
2020-09-26 15:04:01 -0500

    Use/abuse pattern matching for no particular reason

--- idph_stats.rb
+++ idph_stats.rb
@@ -13,25 +13,34 @@ get '/' do
   hospital_data = JSON.parse(HTTParty.get(IDPH_COVID_HOSPITAL_DATA, format: :plain), symbolize_names: true)
   test_data = JSON.parse(HTTParty.get(IDPH_COVID_TEST_DATA, format: :plain), symbolize_names: true)
 
-  region_10_hospitalization = hospital_data[:regionValues].find {|region| region[:id] == 10}.reject! {|k,_| k =~ /^(region|id)$/}
-  state_hospitalization_historic = hospital_data[:HospitalUtilizationResults].map {_1.slice(*SELECT_STATEWIDE_HOSPITALIZATION_DATA)}
+  case hospital_data
+  in  {
+        regionValues: [*, {id: 10, region: _region, **region_10_hospitalization}, *],
+        HospitalUtilizationResults: state_hospitalization_historic
+      }
+    state_hospitalization_historic.map! {|d| d.slice(*SELECT_STATEWIDE_HOSPITALIZATION_DATA)}
+  else
+  end
 
-  test_results = test_data[:historical_county][:values].map do |date|
-    date[:values].filter_map do |county|
-      next unless SELECT_COUNTIES.include? county[:County]
-      county_name = county.delete(:County).downcase
-      county = county_name == 'illinois' ? county.slice(*SELECT_HEADERS.rotate) : county.slice(*SELECT_HEADERS)
-      county.transform_keys! {|k| "#{county_name}_#{k}".to_sym}
-      {date: date[:testDate]}.merge(county)
-    end
-  end.transpose
+  test_results = case test_data
+  in {historical_county: {values: values}}
+    values.map do |date|
+      date[:values].filter_map do |county|
+        next unless SELECT_COUNTIES.include? county[:County]
+        county_name = county.delete(:County).downcase
+        county = (county_name == 'illinois') ? county.slice(*SELECT_HEADERS.rotate) : county.slice(*SELECT_HEADERS)
+        county.transform_keys! {|k| "#{county_name}_#{k}".to_sym}
+        {date: date[:testDate], **county}
+      end
+    end.transpose
+  else
+  end
 
   region_10_table = Thamble.table([region_10_hospitalization.values], {headers: region_10_hospitalization.keys})
   state_hospitalization_historic_table = Thamble.table(state_hospitalization_historic.map(&:values).reverse, {
     headers: state_hospitalization_historic.first.keys,
     table: {id: "hospitalization-data"},
   })
-
 
   test_results_tables = test_results.each_with_index.map do |results, i|
     Thamble.table(results.map(&:values), {
